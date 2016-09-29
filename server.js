e5f9e375478cbe76fbf9871b6555bd27b2815327
Nathan <nathan.dlima@gmail.com>
2016-09-29 21:34:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,54 +9,53 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
-var articleOne = {
-    title : 'Article One | Nathan',
-    heading : 'Article One',
-    date : '29<sup>th</sup> September, 2016',
-    content : `
-        <p>
-            This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. 
-        </p>
-        <p>
-            This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. 
-        </p>
-        <p>
-            This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. 
-        </p>    `
-};
-
-var articleTwo = {
-    title : 'Article Two | Nathan',
-    heading : 'Article Two',
-    date : '29<sup>th</sup> September, 2016',
-    content : `
-        <p>
-            This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. 
-        </p>
-        <p>
-            This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. 
-        </p>
-        <p>
-            This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. 
-        </p>   `
-};
-
-var articleThree= {
-    title : 'Article Three | Nathan',
-    heading : 'Article Three',
-    date : '29<sup>th</sup> September, 2016',
-    content : `
-        <p>
-            This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. 
-        </p>
-        <p>
-            This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. 
-        </p>
-        <p>
-            This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. 
-        </p>    `
-};
+var articles = {
+    articleOne : {
+        title : 'Article One | Nathan',
+        heading : 'Article One',
+        date : '29<sup>th</sup> September, 2016',
+        content : `
+            <p>
+                This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. 
+            </p>
+            <p>
+                This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. 
+            </p>
+            <p>
+                This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. This is the content for article one. 
+            </p>    `
+    },
+    articleTwo : {
+        title : 'Article Two | Nathan',
+        heading : 'Article Two',
+        date : '29<sup>th</sup> September, 2016',
+        content : `
+            <p>
+                This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. 
+            </p>
+            <p>
+                This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. 
+            </p>
+            <p>
+                This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. This is the content for article Two. 
+            </p>   `
+},
+    articleThree : {
+        title : 'Article Three | Nathan',
+        heading : 'Article Three',
+        date : '29<sup>th</sup> September, 2016',
+        content : `
+            <p>
+                This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. 
+            </p>
+            <p>
+                This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. 
+            </p>
+            <p>
+                This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. This is the content for article three. 
+            </p>    `
+}
+}
 
 function createTemplate(data) {
     var title = data.title;
@@ -94,15 +93,15 @@ function createTemplate(data) {
 
 
 app.get('/article-one', function (req, res) {
-    res.send(createTemplate(articleOne));
+    res.send(createTemplate(articles[1]));
 });
 
 app.get('/article-two', function (req, res) {
-    res.send(createTemplate(articleTwo));
+    res.send(createTemplate(articles[2]));
 });
 
 app.get('/article-three', function (req, res) {
-    res.send(createTemplate(articleThree));
+    res.send(createTemplate(articles[3]));
 });
 
 app.get('/ui/style.css', function (req, res) {

fa367747aca37db0bce937f5eda3989982c26e0e
Nathan <nathan.dlima@gmail.com>
2016-09-30 00:03:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,15 +24,34 @@ button.onclick = function() {
 
 //NAME
 var nameip = document.getElementById('name');
-var nameval = nameip.value;
+var name = nameip.value;
 var submit = document.getElementById('submitbtn');
 submit.onclick = function() {
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for(var i=0; i<names.length; i++)
-    {
-        list += '<li>'+names[i]+'</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML=list;
+    //req counter endpoint
+    var req = new XMLHttpRequest();
+    
+    //get response and store it in a var
+    req.onreadystatechange = function() {
+        if(req.readyState === XMLHttpRequest.DONE)
+        {
+            if(req.status === 200)
+            {
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0; i<names.length; i++)
+                {
+                    list += '<li>'+names[i]+'</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML=list;    
+            }
+        }
+    };
+    
+    //make req
+                req.open('GET','http://nate141091.imad.hasura-app.io/submit-name?name='+name,true);
+                req.send(null);
+                
+                
 };

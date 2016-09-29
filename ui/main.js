14b1adb479250e94cee2250e884169cf0dc71bf9
Nathan <nathan.dlima@gmail.com>
2016-09-29 22:00:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,11 @@ var element = document.getElementById('maintxt');
 element.innerHTML = 'Text has been changed!'
 
 var img = document.getElementById('madi');
+function moveRight() {
+    lmargin = lmargin + 10;
+    img.style.marginLeft = lmargin + 'px';
+}
 img.onclick = function() {
     var interval = setInterval(moveRight, 100);
-    //img.align = 'right';
+    //img.align = 'right';  
 }

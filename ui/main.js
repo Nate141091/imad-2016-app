a3a8911840cb72dd3e888efe25c678819fa9f8a9
Nathan <nathan.dlima@gmail.com>
2016-09-29 23:23:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+//COUNTER
 var button = document.getElementById('counter');
 button.onclick = function() {
     //req counter endpoint
@@ -20,3 +21,11 @@ button.onclick = function() {
     req.open('GET','http://nate141091.imad.hasura-app.io/counter',true);
     req.send(null);
 };
+
+//NAME
+var nameip = document.getElementById('name');
+var name = nameip.value;
+var submit = document.getElementById('submitbtn');
+submit.onclick = function() {
+    
+}

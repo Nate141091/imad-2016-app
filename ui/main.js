03bb723f2a93c124984e205089e5737ea0aaa69a
Nathan <nathan.dlima@gmail.com>
2016-09-29 22:54:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,23 @@
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function() {
     //req counter endpoint
+    var req = new XMLHttpRequest();
+    
     
     //get response and store it in a var
+    req.onreadystatechange = function() {
+        if(req.readyState == XMLHttpRequest.DONE)
+        {
+            if(req.status == 200)
+            {
+                var counter = req.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    }
     
-    //put it in the right place
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //make req
+    req.OPEN('GET','http://nate141091.imad.hasura-app.io/counter',true);
+    req.send(null);
 }

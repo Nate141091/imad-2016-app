0242ccae517ed70bf51adadf750e0690e840437b
Nathan <nathan.dlima@gmail.com>
2016-10-03 18:25:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ function createTemplate(data) {
                 ${title}
                 </title>
                 <meta name='viewport' content='width-device-width, initial-scale-1' /> 
-				<link href="profile.css" rel="stylesheet" />
+				<link href="/Profile/profile.css" rel="stylesheet" />
 				<link href="https://fonts.googleapis.com/css?family=Alegreya|Cinzel|Josefin+Slab|Muli|Raleway|Scope+One" rel="stylesheet">
             </head>
             

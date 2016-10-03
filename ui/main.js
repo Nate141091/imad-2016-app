0f330a48cc06852eaa0fd7d7d971143b84de42d0
Nathan <nathan.dlima@gmail.com>
2016-10-03 17:42:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,10 +49,10 @@ submit.onclick = function() {
     };
     
     //make req
-    //var nameip = document.getElementById('name');
-    //var name = nameip.value;
-    var commip = document.getElementById('comment');
-    var comm = commip.value;
-    req.open('GET','http://nate141091.imad.hasura-app.io/submit-comm?comment='+comm,true);
+    var nameip = document.getElementById('name');
+    var name = nameip.value;
+    //var commip = document.getElementById('comment');
+    //var comm = commip.value;
+    req.open('GET','http://nate141091.imad.hasura-app.io/submit-name?name='+comm,true);
     req.send(null);
 };

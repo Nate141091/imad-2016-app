5561df093e5914e9705ebbf759576b0978035aa2
Nathan <nathan.dlima@gmail.com>
2016-10-03 17:42:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/counter', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-comm', function (req, res) {
+app.get('/submit-name', function (req, res) {
     var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
